0cfcea7d974619131ec1afcdec60bd5ceb51fac6
Eric Russell (erussell) <erussell@micron.com>
2016-09-12 15:00:19 -0600

    done with step 1

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ angular
 
 function AppController($scope) {
 
-  $scope.title = 'List Page';
+  $scope.title = 'Heroes';
   
   $scope.heroes = [
     {
@@ -15,5 +15,21 @@ function AppController($scope) {
       affiliations: ['Jedi', 'Rebels'],
       light: true
     },
+    {
+      id: 1,
+      name: 'Darth Vader',
+      imageUrl: 'images/vader.png',
+      power: 9000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: false
+    },
+    {
+      id: 2,
+      name: 'R2D2',
+      imageUrl: 'images/R2D2.png',
+      power: 9000,
+      affiliations: ['Jedi', 'Rebels'],
+      light: true
+    }
   ]
 }
